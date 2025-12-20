0b5c3dcd8068536e0183c0fd489cab314ea27fae
LeoOlivaresD <cspraword@gmail.com>
2025-12-20 20:08:21 -0300

    Integracion de nuevas pantallas en AppScreen

--- app/src/main/java/com/duoc/veterinaria/ui/navigation/AppScreen.kt
+++ app/src/main/java/com/duoc/veterinaria/ui/navigation/AppScreen.kt
@@ -3,10 +3,12 @@ package com.duoc.veterinaria.ui.navigation
 // Define las pantallas disponibles en la app
 enum class AppScreen {
     Splash,
+    Login,
     Welcome,
     Registro,
     Resumen,
     Servicio,
     Provider,
-    BroadcastTest
+    BroadcastTest,
+    AccesoUsuarios
 }
