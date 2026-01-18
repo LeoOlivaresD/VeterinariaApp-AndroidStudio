793d7cf2ac1f715e8d53df393c98cb50f9b3fcfb
LeoOlivaresD <cspraword@gmail.com>
2026-01-18 16:18:23 -0300

    Se comienza a integrar sql lite para registrar intersecciones de clientes

--- app/src/main/java/com/duoc/veterinaria/data/local/sqlite/ClientesLogDbHelper.kt
+++ app/src/main/java/com/duoc/veterinaria/data/local/sqlite/ClientesLogDbHelper.kt
@@ -1,4 +1,35 @@
 package com.duoc.veterinaria.data.local.sqlite
 
-class ClientesLogDbHelper {
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class ClientesLogDbHelper(context: Context) :
+    SQLiteOpenHelper(context, "veterinaria_logs.db", null, 1) {
+
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL(
+            """
+            CREATE TABLE clientes_log(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                evento TEXT NOT NULL,
+                fechaMillis INTEGER NOT NULL
+            )
+            """.trimIndent()
+        )
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        db.execSQL("DROP TABLE IF EXISTS clientes_log")
+        onCreate(db)
+    }
+
+    fun addLog(evento: String) {
+        val values = ContentValues().apply {
+            put("evento", evento)
+            put("fechaMillis", System.currentTimeMillis())
+        }
+        writableDatabase.insert("clientes_log", null, values)
+    }
 }
